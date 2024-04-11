32f8a23419204a3d755de84ec15a51ea73a9c550
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2024-04-11 07:02:05 +0100

    Lesson 3

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,70 @@
 "use strict";
-let myName = "Dave";
-let meaningOfLife;
-let isLoading;
-let album; // any data type, overrides ts
-let postId; // union data type, multiple types
-let isActive;
-let re = /\w+/g; // regex data type
-myName = "John";
-meaningOfLife = 42;
-isLoading = true;
-album = 1984;
-const sum = (a, b) => {
-    return a + b;
+let stringArr = ["one", "hey", "Dave"];
+let guitars = ["Strat", "les Paul", 5150];
+let mixedData = ["evh", 1984, true];
+stringArr[0] = "john";
+stringArr.push("hey");
+guitars[0] = 1984;
+guitars.unshift("Jim");
+guitars = stringArr;
+mixedData = guitars;
+let test = []; // any
+let bands = []; // data type string array
+bands.push("Van Halen");
+// Tuple
+let myTuple = ["Dave", 42, true]; // tuple = template
+let mixed = ["John", 21, false];
+myTuple[1] = 42;
+// Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj); // object type
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: "Dave",
+    prop2: true,
 };
+exampleObj.prop1 = "John";
+// Alternate template
+// interface Guitarist {
+//   // type = object template
+//   name: string
+//   active?: boolean // ? before : optional undefined
+//   albums: (string | number)[] // union type (|) and array []
+// }
+let evh = {
+    // type added to object from type template
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "OU812"],
+};
+let jp = {
+    // type added to object from type template
+    name: "Jimmy",
+    active: true,
+    albums: ["I", "II", "IV"],
+};
+evh = jp;
+// Function
+const greetGuitarist = (guitarist) => {
+    var _a;
+    // parameter type
+    if (guitarist.name) {
+        // narrowing
+        return `Hello ${(_a = guitarist.name) === null || _a === void 0 ? void 0 : _a.toUpperCase()}!`; // ? possible undefined
+    }
+    return "Hello!";
+};
+console.log(greetGuitarist(jp));
+// Enums
+// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime."
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.A); // = 5
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,91 @@
-let myName: string = "Dave"
-let meaningOfLife: number
-let isLoading: boolean
-let album: any // any data type, overrides ts
-let postId: string | number // union data type, multiple types
-let isActive: number | boolean | string
-let re: RegExp = /\w+/g // regex data type
+let stringArr = ["one", "hey", "Dave"]
+let guitars = ["Strat", "les Paul", 5150]
+let mixedData = ["evh", 1984, true]
 
-myName = "John"
-meaningOfLife = 42
-isLoading = true
-album = 1984
+stringArr[0] = "john"
+stringArr.push("hey")
 
-const sum = (a: number, b: number) => {
-  return a + b
+guitars[0] = 1984
+guitars.unshift("Jim")
+
+guitars = stringArr
+mixedData = guitars
+
+let test = [] // any
+let bands: string[] = [] // data type string array
+bands.push("Van Halen")
+
+// Tuple
+let myTuple: [string, number, boolean] = ["Dave", 42, true] // tuple = template
+
+let mixed = ["John", 21, false]
+myTuple[1] = 42
+
+// Objects
+let myObj: object
+
+myObj = []
+console.log(typeof myObj) // object type
+myObj = bands
+myObj = {}
+
+const exampleObj = {
+  prop1: "Dave",
+  prop2: true,
 }
+
+exampleObj.prop1 = "John"
+
+type Guitarist = {
+  // type = object template
+  name?: string
+  active: boolean // ? before : optional undefined
+  albums: (string | number)[] // union type (|) and array []
+}
+
+// Alternate template
+// interface Guitarist {
+//   // type = object template
+//   name: string
+//   active?: boolean // ? before : optional undefined
+//   albums: (string | number)[] // union type (|) and array []
+// }
+
+let evh: Guitarist = {
+  // type added to object from type template
+  name: "Eddie",
+  active: false,
+  albums: [1984, 5150, "OU812"],
+}
+
+let jp: Guitarist = {
+  // type added to object from type template
+  name: "Jimmy",
+  active: true,
+  albums: ["I", "II", "IV"],
+}
+evh = jp
+
+// Function
+const greetGuitarist = (guitarist: Guitarist) => {
+  // parameter type
+  if (guitarist.name) {
+    // narrowing
+    return `Hello ${guitarist.name?.toUpperCase()}!` // ? possible undefined
+  }
+  return "Hello!"
+}
+console.log(greetGuitarist(jp))
+
+// Enums
+// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime."
+
+enum Grade {
+  U = 1,
+  D,
+  C,
+  B,
+  A, // = 5
+}
+
+console.log(Grade.A) // = 5
