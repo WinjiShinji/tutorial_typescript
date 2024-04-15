fa68ee0f7365e593db9ba969146f2db2ebbb7408
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2024-04-15 08:22:22 +0100

    Classes

--- build/js/main.js
+++ build/js/main.js
@@ -1,25 +1,113 @@
 "use strict";
-// convert to more or less specific
-let a = "hello";
-let b = a; // less specific
-let c = a; // more specific
-let d = "world";
-let e = "world";
-const addOrConcat = (a, b, c) => {
-    if (c === "add")
-        return a + b;
-    return "" + a + b;
-};
-let myVal = addOrConcat(2, 2, "concat"); // assertion
-// Be careful!  TS sees no problem - but a string is returned
-let nextVal = addOrConcat(2, 2, "concat"); // assertion
-// 10 as string // incorrect assertion
-10; // double casting | two assertions
-// The DOM
-const img = document.querySelector("img");
-// const img = document.querySelector('img')! // ! = not null
-const myImg = document.getElementById("#img");
-const nextImg = document.getElementById("#img");
-// ^ alternate way <> - does not work with React .tsx
-img.src;
-myImg.src;
+//  ---- CLASSES ---- //
+// Default //
+// class Coder {
+//   name: string
+//   music: string
+//   age: number
+//   lang: string
+//   constructor(
+//     name: string, 
+//     music: string, 
+//     age: number, 
+//     lang: string
+//   ) {
+//     this.name = name
+//     this.music = music
+//     this.age = age
+//     this.lang = lang
+//   }
+// }
+// Class with visible modifiers //
+class Coder {
+    constructor(name, music, age, // private - only accessible within class
+    lang = 'TypeScript' // Optional | 
+    // ^ protected - only accessible within class & subclass
+    ) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
+}
+const Dave = new Coder('Dave', 'Rock', 42);
+console.log(Dave.getAge());
+// console.log(Dave.age) // private - only accessible within class
+// console.log(Dave.lang) // protected - only accessible within class
+// @NOTE: ^ still legal JS - still have access to values in console!
+////////////////////////////////////////////////////////////////////////
+// Class Extends //
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25);
+console.log(Sara.getLang());
+console.log(Sara.getAge());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarist('Jimmy', 'guitar');
+console.log(Page.play('strums'));
+/////////////////////////////////////////////////////////////////////
+// Static Class //
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count; // increments first ++before
+    }
+}
+Peeps.count = 0;
+const John = new Peeps('John');
+const Steve = new Peeps('Steve');
+const Amy = new Peeps('Amy');
+console.log('Amy ID: ', Amy.id);
+console.log('Steve ID: ', Steve.id);
+console.log('John ID: ', John.id);
+console.log('Total: ', Peeps.count);
+////////////////////////////////////////////////////////////////////
+// Class - Getters & Setters //
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error('Param is not an array of strings');
+    }
+}
+const myBands = new Bands();
+myBands.data = ['Neil Young', 'Led Zep'];
+console.log(myBands.data);
+myBands.data = [...myBands.data, 'ZZ Tops'];
+console.log(myBands.data);
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,144 @@
-type One = string
-type Two = string | number
-type Three = "hello"
+//  ---- CLASSES ---- //
 
-// convert to more or less specific
-let a: One = "hello"
-let b = a as Two // less specific
-let c = a as Three // more specific
+// Default //
+// class Coder {
+//   name: string
+//   music: string
+//   age: number
+//   lang: string
 
-let d = <One>"world"
-let e = <string | number>"world"
+//   constructor(
+//     name: string, 
+//     music: string, 
+//     age: number, 
+//     lang: string
+//   ) {
+//     this.name = name
+//     this.music = music
+//     this.age = age
+//     this.lang = lang
+//   }
+// }
 
-const addOrConcat = (
-  a: number,
-  b: number,
-  c: "add" | "concat"
-): number | string => {
-  if (c === "add") return a + b
-  return "" + a + b
+// Class with visible modifiers //
+class Coder {
+  secondLang!: string // not initialized
+
+  constructor(
+    public readonly name: string, 
+    public music: string, 
+    private age: number, // private - only accessible within class
+    protected lang: string = 'TypeScript' // Optional | 
+    // ^ protected - only accessible within class & subclass
+  ) {
+    this.name = name
+    this.music = music
+    this.age = age
+    this.lang = lang
+  }
+  public getAge() {
+    return  `Hello, I'm ${this.age}`
+  }
+}
+const Dave = new Coder('Dave', 'Rock', 42)
+console.log(Dave.getAge())
+// console.log(Dave.age) // private - only accessible within class
+// console.log(Dave.lang) // protected - only accessible within class
+// @NOTE: ^ still legal JS - still have access to values in console!
+////////////////////////////////////////////////////////////////////////
+
+// Class Extends //
+class WebDev extends Coder {
+
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age)
+    this.computer = computer
+  }
+  public getLang() {
+    return `I write ${this.lang}`
+  }
+}
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25)
+console.log(Sara.getLang())
+console.log(Sara.getAge())
+// console.log(Sara.age) // private - only accessible within class
+// console.log(Sara.lang) // protected - only accessible within class
+// @NOTE: ^ still legal JS - still have access to values in console!
+///////////////////////////////////////////////////////////////////////
+
+// Class implements Interface //
+interface Musician {
+  name: string,
+  instrument: string,
+  play(action: string): string
 }
 
-let myVal: string = addOrConcat(2, 2, "concat") as string // assertion
+class Guitarist implements Musician {
+  name: string
+  instrument: string
 
-// Be careful!  TS sees no problem - but a string is returned
-let nextVal: number = addOrConcat(2, 2, "concat") as number // assertion
+  constructor(name: string, instrument: string) {
+    this.name = name
+    this.instrument = instrument
+  }
+  play(action: string){
+    return `${this.name} ${action} the ${this.instrument}`
+  }
+}
+const Page = new Guitarist('Jimmy', 'guitar')
+console.log(Page.play('strums'))
+/////////////////////////////////////////////////////////////////////
 
-// 10 as string // incorrect assertion
-10 as unknown as string // double casting | two assertions
+// Static Class //
+class Peeps {
+  static count: number = 0
 
-// The DOM
-const img = document.querySelector("img") as HTMLImageElement
-// const img = document.querySelector('img')! // ! = not null
-const myImg = document.getElementById("#img") as HTMLImageElement
-const nextImg = <HTMLImageElement>document.getElementById("#img")
-// ^ alternate way <> - does not work with React .tsx
+  static getCount(): number {
+    return Peeps.count
+  }
+  public id: number
+  constructor(public name: string) {
+    this.name = name
+    this.id = ++Peeps.count // increments first ++before
+  }
+}
+const John = new Peeps('John')
+const Steve = new Peeps('Steve')
+const Amy = new Peeps('Amy')
 
-img.src
-myImg.src
+console.log('Amy ID: ',Amy.id)
+console.log('Steve ID: ',Steve.id)
+console.log('John ID: ',John.id)
+console.log('Total: ',Peeps.count)
+////////////////////////////////////////////////////////////////////
+
+// Class - Getters & Setters //
+class Bands {
+  private dataState: string[]
+  
+  constructor(){
+    this.dataState = []
+  }
+
+  public get data(): string[] {
+    return this.dataState
+  }
+
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every(el => typeof el === 'string')){
+      this.dataState = value
+      return 
+    } else throw new Error('Param is not an array of strings')
+  }
+}
+
+const myBands = new Bands()
+myBands.data = ['Neil Young', 'Led Zep']
+console.log(myBands.data)
+myBands.data = [...myBands.data, 'ZZ Tops']
+console.log(myBands.data)
