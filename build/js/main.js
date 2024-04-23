3fa32a7e91c9f56004e5c69f54fa958082a71b63
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2024-04-23 06:32:03 +0100

    Utility Types

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,14 @@
 "use strict";
 // ---- Basics ---- //
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 let myName = "Dave";
 let meaningOfLife;
 let isLoading;
@@ -304,9 +313,75 @@ class StateObj {
     }
 }
 const store = new StateObj("John");
-console.log(store.state);
+// console.log(store.state)
 store.state = "Dave";
 // store.state = 12
 const myState = new StateObj([15]);
 myState.state = (['Dave', 54, true]);
-// console.log(myState.state)
+const updateAssignment = (assign, propsToUpdate // Partial - make props optional
+) => {
+    return Object.assign(Object.assign({}, assign), propsToUpdate);
+};
+const assign1 = {
+    studentId: "comp23",
+    title: "Final Project",
+    grade: 0,
+};
+// console.log(updateAssignment(assign1, { grade: 95 }))
+const assignGraded = updateAssignment(assign1, { grade: 95 });
+// Required & Readonly Utilities //
+const recordAssignment = (assign // Required - make props required
+) => {
+    // send to database, etc
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
+// Record Utility //
+const hexColorMap = {
+    red: "FF0000",
+    green: "00FF00",
+    blue: "0000FF",
+};
+const finalGrades = {
+    Sara: "B",
+    Kelly: "U",
+};
+const gradeData = {
+    Sara: { assign1: 85, assign2: 93 },
+    Kelly: { assign1: 76, assign2: 15 }
+};
+const score = {
+    studentId: "k123",
+    grade: 85,
+};
+const preview = {
+    studentId: "k123",
+    title: "Final Project",
+};
+// Return Utility //
+// type newAssign = { title: string, points: number }
+// const createNewAssign = (title: string, points: number): newAssign => {
+//   return { title, points }
+// }
+const createNewAssign = (title, points) => {
+    return { title, points };
+};
+// Return type - updates based on function return :)
+const tsAssign = createNewAssign("Utility Types", 100);
+const assignArgs = [
+    "Generics",
+    100,
+];
+const tsAssign2 = createNewAssign(...assignArgs);
+const fetchUsers = () => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield fetch('https://jsonplaceholder.typicode.com/users').then(res => {
+        return res.json();
+    }).catch(err => {
+        if (err instanceof Error)
+            console.log(err.message);
+    });
+    return data;
+});
+// fetchUsers().then(users => console.log(users))
+/////////////////////////////////////////////////////////////////
--- src/main.ts
+++ src/main.ts
@@ -502,10 +502,161 @@ class StateObj<T> {
   }
 }
 const store = new StateObj("John")
-console.log(store.state)
+// console.log(store.state)
 store.state = "Dave"
 // store.state = 12
 
 const myState = new StateObj<(string|number|boolean)[]>([15])
 myState.state = (['Dave',54,true])
 // console.log(myState.state)
+//////////////////////////////////////////////////////////////////
+
+// ---- Utility Types ---- //
+
+// Partial Utility //
+interface Assignment {
+  studentId: string,
+  title: string,
+  grade: number,
+  verified?: boolean,
+}
+
+const updateAssignment = (
+  assign: Assignment,
+  propsToUpdate: Partial<Assignment> // Partial - make props optional
+): Assignment => {
+  return { ...assign, ...propsToUpdate }
+}
+
+const assign1: Assignment = {
+  studentId: "comp23",
+  title: "Final Project",
+  grade: 0,
+}
+
+// console.log(updateAssignment(assign1, { grade: 95 }))
+const assignGraded: Assignment = updateAssignment(assign1, { grade: 95 })
+
+// Required & Readonly Utilities //
+
+const recordAssignment = (
+  assign: Required<Assignment> // Required - make props required
+): Assignment => {
+  // send to database, etc
+  return assign
+}
+
+const assignVerified: Readonly<Assignment> = { // Readonly - make props readonly
+  ...assignGraded,
+  verified: true,
+}
+
+recordAssignment({ ...assignGraded, verified: true })
+
+// Record Utility //
+
+const hexColorMap: Record<string, string> = {
+  red: "FF0000",
+  green: "00FF00",
+  blue: "0000FF",
+}
+
+type Students = "Sara" | "Kelly"
+type LetterGrades = "A" | "B" | "C" | "D" | "U"
+
+const finalGrades: Record<Students, LetterGrades> = {
+  Sara: "B",
+  Kelly: "U",
+}
+
+interface Grades {
+  assign1: number,
+  assign2: number
+}
+
+const gradeData: Record<Students, Grades> = {
+  Sara: { assign1: 85, assign2: 93},
+  Kelly: { assign1: 76, assign2: 15 }
+}
+
+// Pick & Omit Utility //
+
+type AssignResult = Pick<Assignment, "studentId" | "grade">
+
+const score: AssignResult = {
+  studentId: "k123",
+  grade: 85,
+}
+
+type AssignPreview = Omit<Assignment, "grade" | "verified">
+
+const preview: AssignPreview = {
+  studentId: "k123",
+  title: "Final Project",
+}
+
+// Exclude & Extract Utility //
+
+type adjGrade = Exclude<LetterGrades, "U">
+type highGrade = Extract<LetterGrades, "A" | "B">
+
+// Nonnullable Utility //
+
+type AllPossibleGrades = 'Dave' | 'John' | null | undefined
+type NamesOnly = NonNullable<AllPossibleGrades>
+
+// Return Utility //
+
+// type newAssign = { title: string, points: number }
+// const createNewAssign = (title: string, points: number): newAssign => {
+//   return { title, points }
+// }
+
+const createNewAssign = (title: string, points: number) => {
+  return { title, points }
+}
+type NewAssign = ReturnType<typeof createNewAssign>
+// Return type - updates based on function return :)
+
+const tsAssign: NewAssign = createNewAssign("Utility Types", 100)
+// console.log(tsAssign)
+
+// Parameter Utility //
+
+type AssignParams = Parameters<typeof createNewAssign>// tuple
+const assignArgs: AssignParams = [
+  "Generics",
+  100,
+]
+const tsAssign2: NewAssign = createNewAssign(...assignArgs)
+// console.log(tsAssign2)
+
+// Awaited Utility //
+// Awaited helps us with the ReturnType of a Promise
+
+interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+}
+
+const fetchUsers = async (): Promise<User[]> => {
+  const data = await fetch(
+    'https://jsonplaceholder.typicode.com/users'
+  ).then(res => {
+    return res.json()
+  }).catch(err => {
+    if (err instanceof Error) console.log(err.message)
+  })
+  return data
+}
+
+// Non-Awaited - Promise<User[]>
+type FetchUsersReturnType1 = ReturnType<typeof fetchUsers>
+
+// Awaited - User[]
+type FetchUsersReturnType2 = Awaited<ReturnType<typeof fetchUsers>>
+
+// fetchUsers().then(users => console.log(users))
+/////////////////////////////////////////////////////////////////
